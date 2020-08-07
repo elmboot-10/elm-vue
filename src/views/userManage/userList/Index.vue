17b8bfcf692bdbec7b36ee41e0f924f52625428d
lyyrhf <448191580@qq.com>
2020-08-07 11:47:25 +0800

    冉海锋--前端

--- src/views/userManage/userList/Index.vue
+++ src/views/userManage/userList/Index.vue
@@ -1,25 +1,30 @@
 <template>
   <div>
     <el-table :data="tableData" border style="width: 100%">
-      <el-table-column prop="userId" label="账号"></el-table-column>
-      <el-table-column prop="userName" label="用户名"></el-table-column>
-      <el-table-column prop label="姓名">
-        <template slot-scope="s">
-          <span v-if="s.row.userSex==1">男</span>
-          <span v-else>女</span>
-        </template>
-      </el-table-column>
-      <el-table-column prop label="图像">
-        <template slot-scope="s">
-          <img :src="s.row.userImg" alt />
-        </template>
-      </el-table-column>
-      <el-table-column fixed="right" label="操作" width="240">
+      <el-table-column prop="usernum" label="序号"></el-table-column>
+      <el-table-column prop="userempno" label="工号"></el-table-column>
+      <el-table-column prop="username" label="姓名"></el-table-column>
+      <el-table-column prop="birth" label="出生年月"></el-table-column>
+      <el-table-column prop="idnum" label="身份证号"></el-table-column>
+      <el-table-column prop="deptno" label="所在部门"></el-table-column>
+      <el-table-column prop="job" label="所在岗位"></el-table-column>
+      <el-table-column prop="entime" label="入职时间"></el-table-column>
+      <el-table-column prop="wtime" label="工作时间"></el-table-column>
+      <el-table-column prop="form" label="员工类型"></el-table-column>
+
+      <el-table-column prop="resource" label="入职来源"></el-table-column>
+   
+      <el-table-column prop="tel" label="电话"></el-table-column>
+      <el-table-column prop="email" label="邮箱"></el-table-column>
+
+
+      <el-table-column fixed="right" label="操作" width="100">
         <div slot-scope="s">
           <el-button type="primary" size="small" @click="routeDemo(s.row)">编辑</el-button>
           <el-button type="danger" size="small" @click="removeItem(s.row)">删除</el-button>
         </div>
       </el-table-column>
+   
     </el-table>
   </div>
 </template>
