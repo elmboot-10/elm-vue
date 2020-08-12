17b60c5c1a8564003493644de77702711a3cd691
代恩 <2714084244@qq.com>
2020-08-12 23:05:24 +0800

    人才库检索

--- src/api/staffEnro/staffEnro.js
+++ src/api/staffEnro/staffEnro.js
@@ -1,6 +1,6 @@
 import request from "@/utils/request.js";
 /**
- * 用户列表
+ * 向后端发送表单内容
  * @param params
  * @returns {AxiosPromise}
  */
@@ -9,6 +9,36 @@ export function dataEntry(params) {
     // closeLoading: true,
     // closeInterceptors: true,
     url: "/staff/dataEntry",
+    method: "post",
+    data: params, //注意：如果是get请求请使用 params: params
+  });
+}
+
+/**
+ * 请求人才库列表
+ * @param params
+ * @returns {AxiosPromise}
+ */
+export function talentlist(params) {
+  return request({
+    // closeLoading: true,
+    // closeInterceptors: true,
+    url: "/talent/list",
+    method: "get",
+    params: params, //注意：如果是get请求请使用 params: params
+  });
+}
+
+/**
+ * 请求人才库列表
+ * @param params
+ * @returns {AxiosPromise}
+ */
+export function talentlistpage(params) {
+  return request({
+    // closeLoading: true,
+    // closeInterceptors: true,
+    url: "/talent/listpage",
     method: "post",
     data: params, //注意：如果是get请求请使用 params: params
   });
--- src/views/staffEnro/labourPool/Index.vue
+++ src/views/staffEnro/labourPool/Index.vue
@@ -1,5 +1,82 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>人才库</span>
+        <el-button style="float: right; padding: 3px 0" type="text">刷新</el-button>
+      </div>
+      <el-table :data="talentpool" style="width: 100%">
+        <el-table-column prop="name" label="姓名" width="100" align="center"></el-table-column>
+        <el-table-column prop="tel" label="电话" width="130" align="center"></el-table-column>
+        <el-table-column prop="email" label="邮箱" align="center"></el-table-column>
+        <el-table-column prop="idNum" label="证件号" align="center"></el-table-column>
+      </el-table>
+      <div class="aaa">
+        <el-pagination
+          small
+          :page-size="pageSize"
+          layout="prev, pager, next"
+          :total="total"
+          @current-change="changePage"
+        ></el-pagination>
+      </div>
+    </el-card>
+  </div>
 </template>
+<style>
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  width: 700px;
+}
+.aaa {
+  width: 100%;
+  text-align: center;
+}
+</style>
+<script>
+import { talentlist } from "@/api/staffEnro/staffEnro";
+export default {
+  data() {
+    return {
+      pageNo: 1,
+      pageSize: 10,
+      total: 0,
+      talentpool: [],
+      searchParmas: {
+        name: "",
+        tel: ""
+      }
+    };
+  },
+  created() {
+    this.initdata();
+  },
+  methods: {
+    initdata() {
+      talentlist()
+        .then(r => {
+          this.talentpool = r;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  }
+};
+</script>
